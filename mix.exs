8aa023397794c61337288fe691f7953f68e41c5a
speeddragon <speeddragon@gmail.com>
2019-12-15 18:29:08 +0000

    Complete mix.exs

--- mix.exs
+++ mix.exs
@@ -7,8 +7,26 @@ defmodule OpenALPR.MixProject do
       version: "0.1.0",
       elixir: "~> 1.6",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      aliases: aliases(),
+      package: [
+        maintainers: ["David Magalhães"],
+        licenses: ["MIT"],
+        links: %{github: "https://github.com/speeddragon/open_alpr"}
+      ],
+      description: """
+      Elixir interface for OpenALPR API.
+      """,
+
+      # Docs
+      name: "Puppeteer PDF",
+      source_url: "https://github.com/speeddragon/open_alpr",
+      docs: [main: "readme", extras: ["README.md"]]
     ]
+  end
+
+  def aliases do
+    [compile: ["compile --warnings-as-errors"]]
   end
 
   def application do
@@ -19,6 +37,7 @@ defmodule OpenALPR.MixProject do
 
   defp deps do
     [
+      {:ex_doc, "~> 0.18", only: :dev},
       {:httpoison, "~> 1.0"},
       {:credo, "~> 1.1.0", only: [:dev, :test], runtime: false},
       {:bypass, "~> 1.0", only: :test}
